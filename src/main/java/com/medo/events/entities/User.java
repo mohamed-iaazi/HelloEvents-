208471e88457d439aa58e1e46e64f17467541f71
mohamed <iaazi.mohamed.1998@gmail.com>
2025-05-21 10:59:46 +0100

    create the user entity with the columns

--- src/main/java/com/medo/events/entities/User.java
+++ src/main/java/com/medo/events/entities/User.java
@@ -1,4 +1,25 @@
 package com.medo.events.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import javax.management.relation.Role;
+
+
+@Entity
+@Data
+
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long userId;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true, nullable = false )
+    private String userName;
+    @Column( nullable = false)
+    private String password;
+    private Long phone;
+    @Column( nullable = false)
+    private Role role;
 }
